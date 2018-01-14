ee07b12f5fb4fb4a03951e2a90a2952b45089a87
Curtis Sapp <curtmsapp@gmail.com>
2018-01-13 18:19:54 -0700

    Randy Branch Auto Practice

---
+++ src/team498/robot/commands/TestingAuto.java
@@ -0,0 +1,31 @@
+package team498.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class TestingAuto extends CommandGroup {
+
+    public TestingAuto() {
+    	addSequential(new DriveStraight(), 2); //drives straight for 2 sec
+    	addSequential(new Stop(), 2); //stop robot for 2 sec
+    	addSequential(new DriveLeft(), 3); //turn left and drive for 3 sec
+    	addSequential(new ReverseForever()); //reverses forever (until button is held down);
+    	addSequential(new ReverseHalfPercent()); //reverses using half the power (until button is not pressed);
+    	addSequential(new Stop());
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    }
+}
