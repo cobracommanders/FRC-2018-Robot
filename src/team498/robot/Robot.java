ab5818861a721af7b83f8f20a0d63fee4e563961
Randy Pham <randy.default@gmail.com>
2018-01-16 15:34:03 -0700

    Added Camera to robot init.

--- src/team498/robot/Robot.java
+++ src/team498/robot/Robot.java
@@ -7,6 +7,8 @@
 
 package team498.robot;
 
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -34,7 +36,7 @@ public class Robot extends TimedRobot {
 	 */
 	@Override
 	public void robotInit() {
-		
+		UsbCamera camera0 = CameraServer.getInstance().startAutomaticCapture("cam0", 0);
 	}
 
 	/**
