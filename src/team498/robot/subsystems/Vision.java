f23f1361188cefbf9e6806e1b26f0d103c7b3da0
Jack Haehl <piginatuxedo4@gmail.com>
2018-01-20 11:51:19 -0700

    Double Camera
    Two cameras with two resolutions possible and USB ports assigned to them.

--- src/team498/robot/subsystems/Vision.java
+++ src/team498/robot/subsystems/Vision.java
@@ -14,13 +14,18 @@ public class Vision extends Subsystem {
 		return vision;
 	}
 	
-	private UsbCamera camera;
-	private int width = 150;
-	private int height = 100;
+	private UsbCamera camera1;
+	private UsbCamera camera2;
+	private int width1 = 150;
+	private int height1 = 100;
+	private int width2 = 150;
+	private int height2 = 100;
 
 	public void startCapture() { 
-		camera = CameraServer.getInstance().startAutomaticCapture("camera", Mappings.CameraDevice);
-		camera.setResolution(width, height);
+		camera1 = CameraServer.getInstance().startAutomaticCapture("camera1", Mappings.CameraDevice1);
+		camera1.setResolution(width1, height1);
+		camera2 = CameraServer.getInstance().startAutomaticCapture("camera2", Mappings.CameraDevice2);
+		camera2.setResolution(width2, height2);
 	}
 	
     public void initDefaultCommand() {
