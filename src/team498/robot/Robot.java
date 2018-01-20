510c5edb3a375e4057c25d66ea153159ed987cb8
Jason <jason@vyrotek.com>
2018-01-19 23:00:15 -0700

    Clean up. Added calls to Robot's UpdateDashboard.

--- src/team498/robot/Robot.java
+++ src/team498/robot/Robot.java
@@ -16,13 +16,6 @@ import team498.robot.subsystems.Drivetrain;
 import team498.robot.subsystems.Gyro;
 import team498.robot.subsystems.Vision;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
- * project.
- */
 public class Robot extends TimedRobot {
 
 	private Operator operator = Operator.getOperator();
@@ -34,71 +27,46 @@ public class Robot extends TimedRobot {
 
 	private Auto auto = new Auto();
 	
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
 	@Override
 	public void robotInit() {
 		vision.startCapture();
+		updateDashboard();
 	}
 
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
 	@Override
-	public void disabledInit() {
-
+	public void disabledInit() {		
 	}
 
 	@Override
 	public void disabledPeriodic() {
+		updateDashboard();
 		Scheduler.getInstance().run();
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
-	 *
-	 * <p>
-	 * You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
-	 */
 	@Override
 	public void autonomousInit() {
+		updateDashboard();
 		this.auto.start();
 	}
 
-	/**
-	 * This function is called periodically during autonomous.
-	 */
 	@Override
 	public void autonomousPeriodic() {
+		updateDashboard();
 		Scheduler.getInstance().run();
 	}
 
 	@Override
 	public void teleopInit() {
+		updateDashboard();
 		this.auto.cancel();
 	}
 
-	/**
-	 * This function is called periodically during operator control.
-	 */
 	@Override
 	public void teleopPeriodic() {
+		updateDashboard();
 		Scheduler.getInstance().run();
 	}
 
-	/**
-	 * This function is called periodically during test mode.
-	 */
 	@Override
 	public void testPeriodic() {
 	}
