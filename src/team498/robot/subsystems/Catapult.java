eefeb33f3101264063973fe855a5294cdc642d7c
Katy Hochstetler <katy@hochstetlers.com>
2018-01-27 10:04:02 -0700

    Added a subsystem and command for the catapult
    added the subsystem catapult and command launch catapult.

---
+++ src/team498/robot/subsystems/Catapult.java
@@ -0,0 +1,33 @@
+package team498.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import team498.robot.Mappings;
+
+
+
+public class Catapult extends Subsystem {
+	DoubleSolenoid catapult; 
+	private static Catapult instance;
+	
+	public static Catapult getInstance() {
+	       instance = instance == null ? new Catapult(Mappings.CatapultForward, Mappings.CatapultReverse) : instance;
+	        return instance;
+	    }
+	private Catapult(int solenoidForward, int solenoidReverse) {
+		catapult = new DoubleSolenoid(solenoidForward, solenoidReverse);
+	}
+	
+	public void launchCatapult() {
+		catapult.set(Value.kForward);
+	}
+	
+	public void resetCatapult() {
+		catapult.set(Value.kOff);
+	}
+    public void initDefaultCommand() {
+        
+    }
+}
+
