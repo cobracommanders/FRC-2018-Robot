c6ae701c59285f9fba06f38f1feb910b5b6aa7fd
Katy Hochstetler <katy@hochstetlers.com>
2018-01-27 12:40:43 -0700

    fixed merge errors

--- src/team498/robot/Robot.java
+++ src/team498/robot/Robot.java
@@ -31,19 +31,13 @@ public class Robot extends TimedRobot {
 	private Vision vision = Vision.getVision();
 	private Gyro gyro = Gyro.getGyro();
 
-	private Auto auto = new Auto();
-	
-	// Stuff
-	private double move;
-	private double rotate;
-
 	// Autonomous Selections
 	SendableChooser<Command> chooser = new SendableChooser<>();
 	Command autonomousCommand;
-	
+
 	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
+	 * This function is run when the robot is first started up and should be used
+	 * for any initialization code.
 	 */
 	@Override
 	public void robotInit() {
@@ -53,7 +47,7 @@ public class Robot extends TimedRobot {
 	}
 
 	@Override
-	public void disabledInit() {		
+	public void disabledInit() {
 	}
 
 	@Override
@@ -67,42 +61,17 @@ public class Robot extends TimedRobot {
 	public void autonomousInit() {
 		// get selected command
 		autonomousCommand = chooser.getSelected();
-		
+
 		// Start autonomous command
 		String gameData;
-		gameData = ds.getGameSpecificMessage(); //not doing anything with game specific message yet
-		
-        if (autonomousCommand != null) {
-        	if (gameData.charAt(0) == 'L') {
-        		autonomousCommand.start();
-        	}
-        }
-            
-        
-        
-    }
-		// TestingAuto auto = new TestingAuto(); //randy's auto
-		/*RotateLeft driveL = new RotateLeft();
-		RotateRight driveR = new RotateRight();
-		DriveStraight driveStraight = new DriveStraight();
-		Stop stop = new Stop();*/
-		
-		// auto.start(); //starts auto
-		 // gets game messgae on driver
-												// station
-		/*if (gameData.charAt(0) == 'L') {
-			driveL.start();
-		} else if (gameData.charAt(0) == 'R') {
-			driveR.start();
-		} else {
-			stop.start();
-		}*/
-		
-		/*if (gameData.charAt(1) == 'L') { //if scale is left, move forward
-			driveStraight.start();
-		} else if (gameData.charAt(1) == 'R') { //if scale is right, move backwards
-			driveReverse.start();
-		}*/
+		gameData = ds.getGameSpecificMessage(); // not doing anything with game specific message yet
+
+		if (autonomousCommand != null) {
+			if (gameData.charAt(0) == 'L') {
+				autonomousCommand.start();
+			}
+		}
+
 		updateDashboard();
 	}
 
@@ -115,8 +84,8 @@ public class Robot extends TimedRobot {
 	@Override
 	public void teleopInit() {
 		if (autonomousCommand != null)
-            autonomousCommand.cancel();
-    }
+			autonomousCommand.cancel();
+
 		updateDashboard();
 	}
 
@@ -129,29 +98,28 @@ public class Robot extends TimedRobot {
 	@Override
 	public void testPeriodic() {
 	}
-	
+
 	private void addAutonomousChoices() {
 
-        // Add available autonomous commands with the SmartDashboard
-        // chooser.addDefault("None", null);
-        //TODO add actual commands to command groups
-        chooser.addDefault("AutoLeft_LeftSwitch", new AutoStartLeft_LeftSwitch());
-        chooser.addObject("AutoLeft_LeftScale", new  AutoStartLeft_LeftScale());
-        chooser.addObject("AutoLeft_RightSwitch", new AutoStartLeft_RightSwitch());
-        chooser.addObject("AutoLeft_RightScale", new  AutoStartLeft_RightScale());
-        chooser.addObject("AutoRight_LeftSwitch", new AutoStartRight_LeftSwitch());
-        chooser.addObject("AutoRight_LeftScale", new  AutoStartRight_LeftScale());
-        chooser.addObject("AutoRight_RightSwitch", new AutoStartRight_RightSwitch());
-        chooser.addObject("AutoRight_RightScale", new  AutoStartRight_RightScale());
-        //chooser.addObject("AutoCrossLine", new AutoCrossLine());
-        SmartDashboard.putData("AutonomousChooser", chooser);
-       // SmartDashboard.putData(Dashboard.AutonomousChooser, chooser);
-    }
-	
-	
+		// Add available autonomous commands with the SmartDashboard
+		// chooser.addDefault("None", null);
+		// TODO add actual commands to command groups
+		chooser.addDefault("AutoLeft_LeftSwitch", new AutoStartLeft_LeftSwitch());
+		chooser.addObject("AutoLeft_LeftScale", new AutoStartLeft_LeftScale());
+		chooser.addObject("AutoLeft_RightSwitch", new AutoStartLeft_RightSwitch());
+		chooser.addObject("AutoLeft_RightScale", new AutoStartLeft_RightScale());
+		chooser.addObject("AutoRight_LeftSwitch", new AutoStartRight_LeftSwitch());
+		chooser.addObject("AutoRight_LeftScale", new AutoStartRight_LeftScale());
+		chooser.addObject("AutoRight_RightSwitch", new AutoStartRight_RightSwitch());
+		chooser.addObject("AutoRight_RightScale", new AutoStartRight_RightScale());
+		// chooser.addObject("AutoCrossLine", new AutoCrossLine());
+		SmartDashboard.putData("AutonomousChooser", chooser);
+		// SmartDashboard.putData(Dashboard.AutonomousChooser, chooser);
+	}
+
 	public void updateDashboard() {
 		operator.updateDashboard();
 		gyro.updateDashboard();
-		//TODO add other subsystems 
+		// TODO add other subsystems
 	}
 }
