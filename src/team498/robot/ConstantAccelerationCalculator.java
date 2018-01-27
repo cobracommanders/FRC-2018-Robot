05dd63316e0c90e1184cf426a338037abd3ebefb
Curtis Sapp <curtmsapp@gmail.com>
2018-01-26 22:32:30 -0700

    Added Acceleration calculator object for move() and turn()

---
+++ src/team498/robot/ConstantAccelerationCalculator.java
@@ -0,0 +1,75 @@
+package team498.robot;
+
+import java.util.Date;
+
+public class ConstantAccelerationCalculator {
+	
+	private double lastValue = 0;
+	private long lastTime = -1;
+	//private double m_RC;
+	private double tN = 0;
+	private double tN1 = 0;
+	private double tN2 = 0;
+	private double c = 0.000002; //change acceleration
+	private double FtN = 0;
+	private double FtN1 = 0;
+	private double FtN2 = 0;
+	
+	public ConstantAccelerationCalculator(double targetAcceleration) {
+		c = targetAcceleration;
+	}
+	
+	public double getNextDataPoint(double targetValue) {
+	    	long currentTime = new Date().getTime();
+			
+			tN = (double)currentTime;
+			
+			if(tN1 == 0) {
+				//nothing
+			} else if (tN2 == 0) {
+				//nothing
+			} else {
+				double a = (tN-tN1) * ((c * (tN - tN2)));
+				double b = ((FtN1 - FtN2)/(tN1 - tN2)) + FtN1;
+				if(FtN1 < targetValue) {
+					FtN = (tN-tN1) * ((c * (tN - tN2)) + ((FtN1 - FtN2)/(tN1 - tN2))) + FtN1;
+					if(FtN > targetValue) {
+						FtN = targetValue;
+					}
+				} else if (FtN1 > targetValue) {
+					// The difference is the negative sign
+					FtN = (tN-tN1) * ((-c * (tN - tN2)) + ((FtN1 - FtN2)/(tN1 - tN2))) + FtN1;
+					//System.out.println("     " + (FtN - FtN1));
+					//System.out.println("FtN:" + FtN + " FtN1: " + FtN1 + " FtN2: " + FtN2 + " tN: " + Math.round((tN * 1000)/1000) + " tN1: " + Math.round((tN1 * 1000)/1000) + " tN2: " + Math.round((tN2 * 1000)/1000));
+					if(FtN < targetValue) {
+						FtN = targetValue;
+					}
+				} else {
+					FtN = targetValue;
+				}
+				
+			} 
+			
+			
+			
+			
+			FtN2 = FtN1;
+			FtN1 = FtN;
+			tN2 = tN1;
+			tN1 = tN;
+			
+			
+			
+			return FtN;
+		
+	}
+	
+	public void enable() {
+		
+	}
+	
+	public void disable() {
+		
+	}
+	
+}
--- src/team498/robot/commands/RampDrive.java
+++ src/team498/robot/commands/RampDrive.java
@@ -1,6 +1,7 @@
 package team498.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import team498.robot.ConstantAccelerationCalculator;
 import team498.robot.Operator;
 import team498.robot.subsystems.Drivetrain;
 import java.util.Date;
@@ -11,18 +12,8 @@ import java.util.Date;
 public class RampDrive extends Command {
 	private Drivetrain drivetrain;
 	private Operator operator = Operator.getOperator();
-	
-	//values for calculateRamp
-	private double lastValue = 0;
-	private long lastTime = -1;
-	//private double m_RC;
-	private double tN = 0;
-	private double tN1 = 0;
-	private double tN2 = 0;
-	private double c = 0.000002; //change acceleration
-	private double FtN = 0;
-	private double FtN1 = 0;
-	private double FtN2 = 0;
+	private ConstantAccelerationCalculator moveAcceleration = new ConstantAccelerationCalculator(0.2);
+	private ConstantAccelerationCalculator turnAcceleration = new ConstantAccelerationCalculator(0.2);
 	
     public RampDrive() {
     	super("RampDrive");
@@ -32,63 +23,14 @@ public class RampDrive extends Command {
         // eg. requires(chassis);
     }
     
-    public void reset() {
-    	lastValue = 0;
-    	lastTime = -1;
-    }
-    
-    public double calculateRamp(double targetValue) {
-    	long currentTime = new Date().getTime();
-		
-		tN = (double)currentTime;
-		
-		if(tN1 == 0) {
-			//nothing
-		} else if (tN2 == 0) {
-			//nothing
-		} else {
-			double a = (tN-tN1) * ((c * (tN - tN2)));
-			double b = ((FtN1 - FtN2)/(tN1 - tN2)) + FtN1;
-			if(FtN1 < targetValue) {
-				FtN = (tN-tN1) * ((c * (tN - tN2)) + ((FtN1 - FtN2)/(tN1 - tN2))) + FtN1;
-				if(FtN > targetValue) {
-					FtN = targetValue;
-				}
-			} else if (FtN1 > targetValue) {
-				// The difference is the negative sign
-				FtN = (tN-tN1) * ((-c * (tN - tN2)) + ((FtN1 - FtN2)/(tN1 - tN2))) + FtN1;
-				//System.out.println("     " + (FtN - FtN1));
-				//System.out.println("FtN:" + FtN + " FtN1: " + FtN1 + " FtN2: " + FtN2 + " tN: " + Math.round((tN * 1000)/1000) + " tN1: " + Math.round((tN1 * 1000)/1000) + " tN2: " + Math.round((tN2 * 1000)/1000));
-				if(FtN < targetValue) {
-					FtN = targetValue;
-				}
-			} else {
-				FtN = targetValue;
-			}
-			
-		} 
-		
-		
-		
-		
-		FtN2 = FtN1;
-		FtN1 = FtN;
-		tN2 = tN1;
-		tN1 = tN;
-		
-		
-		
-		return FtN;
-    }
-    
     // Called just before this Command runs the first time\\
     protected void initialize() {
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        double move = calculateRamp(operator.controller.axisRightTrigger.getAxisValue() - operator.controller.axisLeftTrigger.getAxisValue());
-        double rotate = calculateRamp(operator.controller.axisLeftX.getAxisValue());
+        double move = moveAcceleration.getNextDataPoint(operator.controller.axisRightTrigger.getAxisValue() - operator.controller.axisLeftTrigger.getAxisValue());
+        double rotate = moveAcceleration.getNextDataPoint(operator.controller.axisLeftX.getAxisValue());
         this.drivetrain.drive(move, rotate);
     }
 
