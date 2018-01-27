63cb973fbe41326a747ca99b169641904954bc0d
Randy Pham <randy.default@gmail.com>
2018-01-26 20:57:34 -0700

    Tweaking PID Values
    Training PID

--- src/team498/robot/subsystems/PidAutoTurnSubsystem.java
+++ src/team498/robot/subsystems/PidAutoTurnSubsystem.java
@@ -20,7 +20,7 @@ public class PidAutoTurnSubsystem extends PIDSubsystem {
 	Drivetrain drivetrain = Drivetrain.getDrivetrain();
 
     public PidAutoTurnSubsystem() {
-		super("PidAutoTurnSubsystem", 0.09, 0.05, 0.01); //the PID values are here! TODO CHANGE VALUES FOR OPTIMALNESS
+		super("PidAutoTurnSubsystem", 0.01, 0.1, 0.04); //the PID values are here! TODO CHANGE VALUES FOR OPTIMALNESS
 		setAbsoluteTolerance(1);
 		getPIDController().setContinuous(false);
 		setInputRange(0, 360);
