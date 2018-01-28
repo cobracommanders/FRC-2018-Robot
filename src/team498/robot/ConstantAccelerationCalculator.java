2c0331577a63ac458b837c54473c34faaa3c983d
Randy Pham <randy.default@gmail.com>
2018-01-27 23:40:46 -0700

    Final changes before pull request, cleaned code

--- src/team498/robot/ConstantAccelerationCalculator.java
+++ src/team498/robot/ConstantAccelerationCalculator.java
@@ -6,13 +6,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ConstantAccelerationCalculator {
 	
-	/*private double lastValue = 0;
-	private long lastTime = -1;*/
-	//private double m_RC;
 	private double tN = 0;
 	private double tN1 = 0;
 	private double tN2 = 0;
-	private double c = 0; //change acceleration (was .000002)
+	private double c = 0.02; 
 	private double FtN = 0;
 	private double FtN1 = 0;
 	private double FtN2 = 0;
@@ -26,21 +23,15 @@ public class ConstantAccelerationCalculator {
 			
 			tN = (double)currentTime;
 			
-			if(tN1 == 0) {
-				//nothing
-			} else if (tN2 == 0) {
-				//nothing
-			} else {
+			if (tN1 != 0 && tN2 != 0) {
 				if(FtN1 < targetValue) {
 					FtN = (tN-tN1) * ((c * (tN - tN2)) + ((FtN1 - FtN2)/(tN1 - tN2))) + FtN1;
 					if(FtN > targetValue) {
 						FtN = targetValue;
 					}
 				} else if (FtN1 > targetValue) {
-					// The difference is the negative sign
+					// The difference is the negative c
 					FtN = (tN-tN1) * ((-c * (tN - tN2)) + ((FtN1 - FtN2)/(tN1 - tN2))) + FtN1;
-					//System.out.println("     " + (FtN - FtN1));
-					//System.out.println("FtN:" + FtN + " FtN1: " + FtN1 + " FtN2: " + FtN2 + " tN: " + Math.round((tN * 1000)/1000) + " tN1: " + Math.round((tN1 * 1000)/1000) + " tN2: " + Math.round((tN2 * 1000)/1000));
 					if(FtN < targetValue) {
 						FtN = targetValue;
 					}
@@ -49,30 +40,24 @@ public class ConstantAccelerationCalculator {
 				}
 				
 			} 
-			
-			
-			
-			
+	
 			FtN2 = FtN1;
 			FtN1 = FtN;
 			tN2 = tN1;
 			tN1 = tN;
-			
-			
-			
+
 			return FtN;
-		
 	}
 	
-	public void updateDashboard() {
-		SmartDashboard.putNumber("c (constant acceleration value): ", c);
-		SmartDashboard.putNumber("Difference between two points", (FtN1 - FtN2));
-		SmartDashboard.putNumber("FtN: ", FtN);
-		SmartDashboard.putNumber("FtN1: " , FtN1);
-		SmartDashboard.putNumber("FtN2: " , FtN2);
-		SmartDashboard.putNumber("tN: " , Math.round((tN * 1000)/1000));
-		SmartDashboard.putNumber("tN1: " , Math.round((tN1 * 1000)/1000));
-		SmartDashboard.putNumber("tN2: " , Math.round((tN2 * 1000)/1000));
+	public void printDebug() {
+		System.out.println("c (constant acceleration value): "+ c);
+		System.out.println("Difference between two points"+ (FtN1 - FtN2));
+		System.out.println("FtN: " + FtN);
+		System.out.println("FtN1: " + FtN1);
+		System.out.println("FtN2: " + FtN2);
+		System.out.println("tN: " + Math.round((tN * 1000)/1000));
+		System.out.println("tN1: " + Math.round((tN1 * 1000)/1000));
+		System.out.println("tN2: " + Math.round((tN2 * 1000)/1000));
 	}
 	
 }
--- src/team498/robot/commands/RampDrive.java
+++ src/team498/robot/commands/RampDrive.java
@@ -19,18 +19,12 @@ public class RampDrive extends Command {
     	super("RampDrive");
     	
     	requires(this.drivetrain = Drivetrain.getDrivetrain());
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
     }
-    
-    // Called just before this Command runs the first time\\
+
     protected void initialize() {
     }
 
-    // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	moveAcceleration.updateDashboard();
-    	//turnAcceleration.updateDashboard();
     	
         double move = moveAcceleration.getNextDataPoint(operator.controller.axisRightTrigger.getAxisValue() - operator.controller.axisLeftTrigger.getAxisValue());
         double rotate = turnAcceleration.getNextDataPoint(operator.controller.axisLeftX.getAxisValue());
@@ -40,18 +34,14 @@ public class RampDrive extends Command {
         SmartDashboard.putNumber("rotate value", rotate);
     }
 
-    // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         return false;
     }
 
-    // Called once after isFinished returns true
     protected void end() {
     	drivetrain.drive(0, 0);
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     protected void interrupted() {
     	end();
     }
