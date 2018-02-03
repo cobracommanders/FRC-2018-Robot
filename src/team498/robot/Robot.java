3273fb02a5e75d24ba0f4aebfaece5ab27cf93a0
Unknown <micalobia2@gmail.com>
2018-02-02 20:18:51 -0700

    Formatting
    It counted a formatting as a change in the whole doc, need to commit before merging from master

--- src/team498/robot/Robot.java
+++ src/team498/robot/Robot.java
@@ -49,7 +49,7 @@ public class Robot extends TimedRobot {
     @Override
     public void disabledInit() {
     }
-
+    
     @Override
     public void disabledPeriodic() {
         updateDashboard();
