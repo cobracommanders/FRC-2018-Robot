e823e3a8ef755e01f5e24e0af91563225f834412
Unknown <micalobia2@gmail.com>
2018-02-03 11:25:49 -0700

    Updated algorithm
    Made it actually use abs, and it should work (hopefully)

--- src/team498/robot/commands/RampDrive.java
+++ src/team498/robot/commands/RampDrive.java
@@ -16,44 +16,48 @@ public class RampDrive extends Command {
 	private Gyro gyro;
 	private ConstantAccelerationCalculator moveAcceleration = new ConstantAccelerationCalculator(0.0005);
 	private ConstantAccelerationCalculator turnAcceleration = new ConstantAccelerationCalculator(0.0005);
-	
-    public RampDrive() {
-    	super("RampDrive");
-    	
-    	requires(this.drivetrain = Drivetrain.getDrivetrain());
-    	requires(this.gyro = Gyro.getGyro());
-    }
 
-    protected void initialize() {
-    }
-    
+	public RampDrive() {
+		super("RampDrive");
+
+		requires(this.drivetrain = Drivetrain.getDrivetrain());
+		requires(this.gyro = Gyro.getGyro());
+	}
+
+	protected void initialize() {
+	}
+
 	public double AngleComp() {
-		if (operator.controller.axisRightTrigger.getAxisValue() >= 0.2 || operator.controller.axisLeftTrigger.getAxisValue() >= 0.2) {
-			return -gyro.getAngleZ() / 9;
-		} else {
+		if (Math.abs(operator.controller.axisLeftX.getAxisValue()) > 0.1) {
+			gyro.reset();
 			return 0;
 		}
+		
+		return gyro.getAngleZ() / 45; //Should make to go between -1 and 1, unless it goes past 45 without correcting which is a problem
 	}
-    
-    protected void execute() {
-    	
-        double move = moveAcceleration.getNextDataPoint(operator.controller.axisRightTrigger.getAxisValue() - operator.controller.axisLeftTrigger.getAxisValue());
-        double rotate = turnAcceleration.getNextDataPoint(operator.controller.axisLeftX.getAxisValue());
-        this.drivetrain.drive(move, rotate);
-        
-        SmartDashboard.putNumber("move value", move);
-        SmartDashboard.putNumber("rotate value", rotate);
-    }
 
-    protected boolean isFinished() {
-        return false;
-    }
+	protected void execute() {
 
-    protected void end() {
-    	drivetrain.drive(0, 0);
-    }
+		double move = moveAcceleration.getNextDataPoint(operator.controller.axisRightTrigger.getAxisValue()
+				- operator.controller.axisLeftTrigger.getAxisValue());
+		double rotate = turnAcceleration.getNextDataPoint(operator.controller.axisLeftX.getAxisValue());
+		double comp = AngleComp();
+		System.out.println(comp);
+		this.drivetrain.drive(move, rotate);
 
-    protected void interrupted() {
-    	end();
-    }
+		SmartDashboard.putNumber("move value", move);
+		SmartDashboard.putNumber("rotate value", rotate + comp);
+	}
+
+	protected boolean isFinished() {
+		return false;
+	}
+
+	protected void end() {
+		drivetrain.drive(0, 0);
+	}
+
+	protected void interrupted() {
+		end();
+	}
 }
