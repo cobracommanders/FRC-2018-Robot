2806655ef315794d052ac86a68097fea01a4125f
Jason <jason@vyrotek.com>
2018-02-03 18:11:59 -0700

    Cleanup

--- src/team498/robot/subsystems/Vision.java
+++ src/team498/robot/subsystems/Vision.java
@@ -5,12 +5,8 @@ import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import team498.robot.Mappings;
 
-
 public class Vision extends Subsystem {
-	
-	public static final int Width = 150;
-	public static final int Height = 100;
-	
+		
 	private static Vision vision = null;
 	public static Vision getVision() {
 		vision = vision == null ? new Vision() : vision;
@@ -31,8 +27,7 @@ public class Vision extends Subsystem {
 		camera2.setResolution(width2, height2);
 	}
 	
-    public void initDefaultCommand() {
-    	
+    public void initDefaultCommand() {    	
     }
     
 }
