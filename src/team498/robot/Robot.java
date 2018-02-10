383c706d9e38d724ee0dc5a3feb94a8b4cf67bae
Randy Pham <randy.default@gmail.com>
2018-02-10 10:43:43 -0700

    Fixed robot crash bug that was on master, and updated vision
    deleted .setContinous and that fixed the robot code crash

--- src/team498/robot/Robot.java
+++ src/team498/robot/Robot.java
@@ -57,7 +57,7 @@ public class Robot extends TimedRobot {
 
 	@Override
 	public void robotInit() {
-		//vision.startCapture();
+		vision.startCapture();
 		updateDashboard();
 		addAutonomousChoices();
 	}
