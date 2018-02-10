6ff465e5e083f7cc8bcf72e4c544cb35584f6509
Unknown <micalobia2@gmail.com>
2018-02-10 15:18:29 -0700

    Added turbo code

--- src/team498/robot/commands/ManualDrive.java
+++ src/team498/robot/commands/ManualDrive.java
@@ -7,11 +7,15 @@ import team498.robot.Operator;
 import team498.robot.subsystems.Drivetrain;
 
 public class ManualDrive extends Command {
-	
+
 	private Operator operator = Operator.getOperator();
 	private Drivetrain drivetrain;
 	private ConstantAccelerationCalculator moveAcceleration = new ConstantAccelerationCalculator(0.0005);
 	private ConstantAccelerationCalculator turnAcceleration = new ConstantAccelerationCalculator(0.0005);
+	private boolean oldState = false;
+	private boolean newState = false;
+	private boolean turbo = false;
+	private double turboCap = 0.8; // TODO: change, maybe?
 
 	public ManualDrive() {
 		super("RampDrive");
@@ -24,13 +28,21 @@ public class ManualDrive extends Command {
 
 	protected void execute() {
 
-		double move = moveAcceleration.getNextDataPoint(operator.controller.axisRightTrigger.getAxisValue()	- operator.controller.axisLeftTrigger.getAxisValue());
+		oldState = newState;
+		newState = operator.controller.buttonY.get();
+		if (newState && !oldState) {
+			turbo = !turbo;
+		}
+
+		double move = moveAcceleration.getNextDataPoint(operator.controller.axisRightTrigger.getAxisValue()
+				- operator.controller.axisLeftTrigger.getAxisValue());
 		double rotate = turnAcceleration.getNextDataPoint(operator.controller.axisLeftX.getAxisValue());
 
 		this.drivetrain.drive(move, rotate);
 
-		SmartDashboard.putNumber("move value", move);
+		SmartDashboard.putNumber("move value", move * (turbo ? 1 : turboCap));
 		SmartDashboard.putNumber("rotate value", rotate);
+		SmartDashboard.putBoolean("Turbo mode", turbo);
 	}
 
 	protected boolean isFinished() {
