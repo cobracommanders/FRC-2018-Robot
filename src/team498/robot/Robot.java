30bfbc44105b56072b7dda7aa88caefac1fba547
Randy Pham <randy.default@gmail.com>
2018-02-09 20:18:30 -0700

    small update because merge failed
    AutoTurn file got deleted during merge, so i reverted the commit. Thanks GitKraken!! :D

--- src/team498/robot/Robot.java
+++ src/team498/robot/Robot.java
@@ -9,7 +9,6 @@ package team498.robot;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
