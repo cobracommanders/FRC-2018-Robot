d2b94b574b4c28781dc866cf212c752a0099e9db
Katy Hochstetler <katy@hochstetlers.com>
2018-02-17 14:07:12 -0700

    fixed auto arm
    I made it so it doesnt get stuck trying to get the exact arm position

--- src/team498/robot/commands/auto/common/AutoArmPosition.java
+++ src/team498/robot/commands/auto/common/AutoArmPosition.java
@@ -7,57 +7,59 @@ import team498.robot.subsystems.Arm;
  *
  */
 public class AutoArmPosition extends Command {
-	
+
 	private Arm arm;
-	
+
 	private double armPower = .6;
-	private double targetArmPosition; //from 0 to 315 degrees
+	private double targetArmPosition; // from 0 to 315 degrees
 	private double armPosition;
-	
+
+	private boolean higherThanTarget = false;
 	private boolean done = false;
-	
-    public AutoArmPosition(double targetArmPosition) {
-    	super("AutoArmPosition");
-    	
-    	requires(this.arm = Arm.getArm());
-    	
-    	this.targetArmPosition = targetArmPosition;
-    	
-    	armPosition = arm.getPosition();
-    }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	public AutoArmPosition(double targetArmPosition) {
+		super("AutoArmPosition");
+		requires(this.arm = Arm.getArm());
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
+		this.targetArmPosition = targetArmPosition;
+		this.armPosition = arm.getPosition();
+		this.higherThanTarget = armPosition > targetArmPosition;
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
 		// arm with potentiometer
-		if (armPosition < targetArmPosition) {
-			arm.setArm(armPower);
-		} else if (armPosition > targetArmPosition) {
+		if (higherThanTarget) {
 			arm.setArm(-armPower);
-		} else{
-			arm.setArm(0);
-			done = true;
-			
-		}
-		
-		
-    }
+			if(arm.getPosition() <= targetArmPosition) {
+				done = true;
+			}
+		} else if(!higherThanTarget) {
+			arm.setArm(armPower);
+			if(arm.getPosition() >= targetArmPosition) {
+				done = true;
+			}
+		} 
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return done;
-    }
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return done;
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	this.end();
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+		arm.setArm(0);
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		this.end();
+	}
 }
