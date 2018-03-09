a1d5674bb7cad4fb33c55869705a2456c2bae91e
Randy Pham <randy.default@gmail.com>
2018-03-08 17:11:38 -0700

    Temporary (Hopefully) Autonomous Commands
    These commands use time and gyro respectively to move the arm and turn the robot in place of the currently incomplete PID loops to control these things.

---
+++ src/team498/robot/commands/auto/common/AutoArmTimed.java
@@ -0,0 +1,44 @@
+package team498.robot.commands.auto.common;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+import team498.robot.subsystems.Arm;
+
+/**
+ *
+ */
+public class AutoArmTimed extends Command {
+	public double time;
+	public double armPower;
+	private Arm arm;
+
+	private Timer timer = new Timer();
+
+	public AutoArmTimed(double armPower, double time) {
+		super("AutoDriveTimed");
+		requires(this.arm = Arm.getArm());
+
+		this.armPower = armPower;
+		this.time = time;
+	}
+
+	protected void initialize() {
+		timer.start();
+	}
+
+	protected void execute() {
+		arm.setArmPower(armPower);
+	}
+
+	protected boolean isFinished() {
+		return timer.get() >= time;
+	}
+
+	protected void end() {
+		arm.setArmPower(0);
+	}
+
+	protected void interrupted() {
+		end();
+	}
+}
---
+++ src/team498/robot/commands/auto/common/AutoOldTurn.java
@@ -0,0 +1,46 @@
+package team498.robot.commands.auto.common;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+import team498.robot.subsystems.ADIS16448_IMU;
+import team498.robot.subsystems.Drivetrain;
+
+/**
+ *
+ */
+public class AutoOldTurn extends Command {
+	public double gyroGoal;
+	public double turnPower;
+	private Drivetrain drivetrain;
+	private ADIS16448_IMU gyro = new ADIS16448_IMU();
+
+	private Timer timer = new Timer();
+
+	public AutoOldTurn(double turnPower, double gyroGoal) {
+		super("AutoOldTurn");
+		requires(this.drivetrain = Drivetrain.getDrivetrain());
+
+		this.gyroGoal = gyroGoal;
+		this.turnPower = turnPower;
+	}
+
+	protected void initialize() {
+		gyro.reset();
+	}
+
+	protected void execute() {
+		drivetrain.manualDrive(0, turnPower);
+	}
+
+	protected boolean isFinished() {
+		return Math.abs(gyro.getAngleX()) >= Math.abs(gyroGoal);
+	}
+
+	protected void end() {
+		drivetrain.manualDrive(0, 0);
+	}
+
+	protected void interrupted() {
+		end();
+	}
+}
